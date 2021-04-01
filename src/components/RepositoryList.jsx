49215fb4cda4169f5f52fb0d78715f8d3e2ef168
Vyctoria <vyctoriak@gmail.com>
2021-04-01 18:25:45 -0300

    link in repository item component, call github public api and iterating, created user component to get the name of the owner repos, and scss of user component and repository component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,45 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { User } from './User';
 
-const repository = {
-  name: "Unform 1",
-  description: "Forms in React",
-  link: "https://github.com/vyctoriak/BeTheHero"
-}
+// https://api.github.com/users/samuel1992/repos
 
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([])
+
+  /* 
+  * IMPORTANTE:
+   dispara uma função quando alguma variável muda (se setada entre o [] de dependência), 
+   caso o [] esteja vazio a função é executada uma única vez
+  */
+  
+  /*
+  ! CUIDADO :
+    Se deixar o segundo parâmetro (o array de dependência) vazio,
+    o useEffect irá entrar em looping!
+  */
+
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/samuel1992/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
+  // console.log(repositories)
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
+      <h3>Respositórios do usuário: <User/> </h3>
+
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem repository={repository} />
+        })}
       </ul>
     </section>
   )
